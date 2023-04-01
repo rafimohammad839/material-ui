c3f9e57924a583b304c8a4f53b9ec007a0315586
Mohammad Rafi <rafi.mohammad@autodesk.com>
2023-04-01 13:22:48 +0530

    Final commit

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,6 +1,7 @@
 import { Container, Grid, Paper } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import NoteCard from "../components/NoteCard";
+import Masonry from "react-masonry-css";
 
 const Notes = () => {
   const [notes, setNotes] = useState([]);
@@ -28,15 +29,25 @@ const Notes = () => {
     }
   };
 
+  const breakpoints = {
+    default: 3,
+    1100: 2,
+    700: 1
+  }
+
   return (
     <Container>
-      <Grid container spacing={3}>
+      <Masonry
+        breakpointCols={breakpoints}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
         {notes.map((note) => (
-          <Grid key={note.id} item xs={12} md={6} lg={4}>
+          <div key={note.id}>
             <NoteCard note={note} handleDelete={handleDelete} />
-          </Grid>
+          </div>
         ))}
-      </Grid>
+      </Masonry>
     </Container>
   );
 };
